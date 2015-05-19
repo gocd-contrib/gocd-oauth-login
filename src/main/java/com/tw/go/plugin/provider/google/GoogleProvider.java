f3a4078395ca3bbcd829dfe3a26c7e0129e390e3
Srini <srinivas.upadhya@gmail.com>
2015-05-19 20:31:06 +0530

    add support for google oauth

---
+++ src/main/java/com/tw/go/plugin/provider/google/GoogleProvider.java
@@ -0,0 +1,39 @@
+package com.tw.go.plugin.provider.google;
+
+import com.tw.go.plugin.User;
+import com.tw.go.plugin.provider.Provider;
+import org.brickred.socialauth.Profile;
+
+public class GoogleProvider implements Provider {
+    @Override
+    public String getPluginId() {
+        return "google.oauth.login";
+    }
+
+    @Override
+    public String getName() {
+        return "Google";
+    }
+
+    @Override
+    public String getProviderName() {
+        return "googleplus";
+    }
+
+    @Override
+    public String getConsumerKeyPropertyName() {
+        return "googleapis.com.consumer_key";
+    }
+
+    @Override
+    public String getConsumerSecretPropertyName() {
+        return "googleapis.com.consumer_secret";
+    }
+
+    @Override
+    public User getUser(Profile profile) {
+        String emailId = profile.getEmail();
+        String fullName = profile.getFullName();
+        return new User(emailId, fullName, emailId);
+    }
+}
